0a28d9f491f503e528df2267b8cac675dc5f1f29
nathanaelA <nathanael.allen1@gmail.com>
2024-08-30 21:09:31 -0700

    added more formatting

--- views/finishedBooks.js
+++ views/finishedBooks.js
@@ -5,31 +5,40 @@ async function getFinishedBooks(){
     let bookDivs = /*html*/`
     `;
 
-    let mainList = /*html*/`
-        <h4 class="font-bold text-lg">Finished Books</h4>
+    let header = /*html*/`
+        <h4 class="font-bold text-lg text-center">Finished Books</h4> 
     `;
 
     books.forEach((book)=>{
         let html = /*html*/`
         <li>
-            <div class="my-2">
-                <p class="font-semibold text-lg">Title: ${book['TITLE']}</p>
-                <p class="font-semibold">Author: ${book['AUTHOR']}</p>
-                <p>Rating: ${book['RATING'] ? book['RATING'] : 'no-rating'}/10</p>
-                <p>Review: ${book['REVIEW'] ? book['REVIEW'] : ''}</p>
+            <div class="max-sm:block my-2 border border-gray-400 rounded-md bg-gray-800 grid grid-cols-2 h-48 my-7">
+                <div>
+                    <p class="font-semibold text-lg col-start-1">Title: ${book['TITLE']}</p>
+                    <p class="col-start-1">Author: ${book['AUTHOR']}</p>
+                    <p class="col-start-1">Rating: ${book['RATING'] ? book['RATING'] : 'no-rating'}/10</p>
+                </div>
+                <p class="col-start-2 row-start-1 overflow-scroll">Review: ${book['REVIEW'] ? book['REVIEW'] : ''}</p>
             </div>
         </li>
         `;
         bookDivs += html;
     })
     
-    mainList += /*html*/`
-        <ul>
-            ${bookDivs}
-        </ul> 
+    let bookList = /*html*/`
+    <ul class="max-lg:block ">
+        ${bookDivs}
+    </ul> 
     `;
 
-    return mainList
-}
+    let mainDiv = /*html*/`
+        <div class="flex flex-col md:w-4/5 justify-center items-center md:m-auto">
+            ${header}
+            ${bookList}
+        </div>
+    `
+
+    return mainDiv
+} 
 
 export {getFinishedBooks}
